4ca1de7147685241947659cdeae85e6d8927d897
Mehrdad Soltani <m-soltani@users.noreply.github.com>
2024-01-20 11:06:44 -0600

    add new endpoint

--- src/Program.cs
+++ src/Program.cs
@@ -36,6 +36,12 @@ app.MapGet("/albums", () =>
 })
 .WithName("GetAlbums");
 
+app.MapGet("/healthz", () =>
+{
+    return Album.GetAll();
+})
+.WithName("GetHealthz");
+
 app.Run();
 
 record Album(int Id, string Title, string Artist, double Price, string Image_url)
@@ -50,6 +56,6 @@ record Album(int Id, string Title, string Artist, double Price, string Image_url
             new Album(6, "Sweet Container O' Mine", "Guns N Probeses", 14.99, "https://aka.ms/albums-containerappslogo")
          };
 
-        return albums; 
+        return albums;
      }
 }
